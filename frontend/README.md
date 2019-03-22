94f1d35d6927ee94238089dacd6b0e41cb166aee
Kyle Reid <kyreid14@gmail.com>
2019-03-22 15:06:43 -0400

    frontend updated
    updated front end to master branch with backend

--- frontend/README.md
+++ frontend/README.md
@@ -1,36 +1,6 @@
-# Remind-It
+# RemindIt
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.2.
-
-## Description
-Location based to-do list application that has the user register and request access to location. input a to-do list to the application
-and it will mark as complete once user goes to location.
-
-## Installation & Libraries
-
-#### Libraries
-
-## Usage
-
-
-## Authors and acknowledgment
-Kenneth Sauers - Project Manager & Database  
-Kyle Reid - Front-end  
-Christopher Dowlatram - Front-end  
-Misty Au - API  
-Jose Luis - Database  
-Mykola Maslych - Database  
-Jorge Nunez - Javascript/API  
-
-## Contributing
-For Major changes, PLease open an issue first to discuss what 
-you would like to change.
-
-Please make sure to update tests as appropriate
-
-## License
-
-**NONE**
 
 ## Development server
 
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// different pages of the website
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
 import { RegisterComponent } from './register/register.component';
 import { UserpageComponent } from './userpage/userpage.component';
+import { MapsComponent } from './maps/maps.component';
+import { EventsComponent } from './events/events.component';
 
 
 const routes: Routes = [
@@ -15,6 +18,8 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'userpage', component: UserpageComponent },
+  { path: 'maps', component: MapsComponent },
+  { path: 'events', component: EventsComponent },
 
 
   // redirect to home

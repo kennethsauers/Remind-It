5a34cb39e71467fc21d3fc31f64d9fc03f3abf2a
Jose Luis <jose@Joses-Mac-mini.local>
2019-04-13 11:23:23 -0400

    Fixed the create reminder modal some
    typescript. ugh.
    L#53 if (time.hour) is not a good check for null because if hour was 0
    it would return false and not set an hour
    
    Removed the button for the date picker so it can be in-line with time
    Now when you click the field it opens the calendar
    
    Gonna work on loading up the reminders now

--- frontend/src/app/create-reminder/create-reminder.component.ts
+++ frontend/src/app/create-reminder/create-reminder.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {debounceTime} from 'rxjs/operators';
+import { debounceTime } from 'rxjs/operators';
 
 import { CreateReminderInformation, CreateReminderResponse } from '../schema/reminders';
 import { AuthenticationService } from '../services/authentication.service';
@@ -15,20 +15,21 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./create-reminder.component.css']
 })
 export class CreateReminderComponent implements OnInit {
+  openedDate: Date = new Date();
   createReminderForm = new FormGroup({
-    name: new FormControl('birthday party my dude', [
+    name: new FormControl('Name', [
       Validators.required,
       Validators.maxLength(32)
     ]),
-    description: new FormControl('call your friends', [
+    description: new FormControl('Description', [
       Validators.required,
       Validators.maxLength(32767)
     ]),
-    dueDate: new FormControl(new Date(), [
+    dueDate: new FormControl({ month: this.openedDate.getMonth() + 1, day: this.openedDate.getDate(), year: this.openedDate.getFullYear() }, [
       Validators.required,
       Validators.maxLength(16)
     ]),
-    time: new FormControl('', Validators.maxLength(16)),
+    time: new FormControl({ hour: this.openedDate.getHours(), minute: this.openedDate.getMinutes(), second: 0 }, Validators.maxLength(16)),
     isPublic: new FormControl(false),
     isRepeating: new FormControl(false)
   });
@@ -38,19 +39,19 @@ export class CreateReminderComponent implements OnInit {
   message: String;
 
   constructor(private authService: AuthenticationService,
-              private eventService: EventService,
-              public activeModal: NgbActiveModal,
-              private router: Router) { }
-              
+    private eventService: EventService,
+    public activeModal: NgbActiveModal,
+    private router: Router) { }
+
   ngOnInit() {
-    this._success.subscribe( (msg) => { this.message = msg });
+    this._success.subscribe((msg) => { this.message = msg });
     this._success.pipe(
       debounceTime(5000)
-    ).subscribe( () => this.success = null );
+    ).subscribe(() => this.success = null);
   }
 
   getDateFromPicker(date: any, time?: any): Date {
-    if (time.hour)
+    if (time.hour != null)
       return new Date(date.year, date.month - 1, date.day, time.hour, time.minute, time.second);
     else
       return new Date(date.year, date.month - 1, date.day);
@@ -64,11 +65,11 @@ export class CreateReminderComponent implements OnInit {
       repeats: this.createReminderForm.get('isRepeating').value,
       name: this.createReminderForm.get('name').value,
       description: this.createReminderForm.get('description').value,
-      dueDate: this.getDateFromPicker(this.createReminderForm.get('dueDate').value, 
-                                      this.createReminderForm.get('time').value)
+      dueDate: this.getDateFromPicker(this.createReminderForm.get('dueDate').value,
+        this.createReminderForm.get('time').value)
     };
 
-    this.eventService.addReminder(this.authService.getToken(), reminderInformation).subscribe( (res: CreateReminderResponse) => {
+    this.eventService.addReminder(this.authService.getToken(), reminderInformation).subscribe((res: CreateReminderResponse) => {
       console.log(res.event);
       this.success = res.success;
       this._success.next(res.msg);
